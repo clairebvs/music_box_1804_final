a96924fc330ab144a6139d24f5aefea94162bce2
ClaireBvs <clairembeauvais@gmail.com>
2018-07-31 09:11:50 -0600

    Add join table genre song and relationships

--- spec/models/genre_song_spec.rb
+++ spec/models/genre_song_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe GenreSong, type: :model do
   describe 'relationships' do
-    it{should belong_to(:genres)}
-    it{should belong_to(:songs)}
+    it{should belong_to(:genre)}
+    it{should belong_to(:song)}
   end
 end
